917fbd1602365dfc1bd5e86154be3e692b4c1ab6
Kia Farhang <kfarhang0@gmail.com>
2017-05-28 15:27:50 -0500

    ejected from cr create react app, added basic message sanitization on server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const xssFilters = require('xss-filters');
+
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+		io.emit('chat message', xssFilters.inHTMLData(msg));
 	});
 });
 
